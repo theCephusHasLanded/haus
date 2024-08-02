57d8fe7cf49b2e89008434c829807c9431385bde
christinacephus@pursuit.org <christinacephus@pursuit.org>
2024-08-02 00:38:39 -0400

    logos

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,22 @@
-import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Box, Text, Image, Flex, useColorModeValue } from '@chakra-ui/react';
 
 const Footer = () => {
+  const bgColor = useColorModeValue('gray.100', 'gray.900');
+  const textColor = useColorModeValue('gray.800', 'gray.200');
+
   return (
-    <Box as="footer" py={4} textAlign="center" bg="gray.100">
-      <Text>&copy; {new Date().getFullYear()} HAUS. LKHN Technologies. All rights reserved.</Text>
+    <Box as="footer" py={6} textAlign="center" bg={bgColor}>
+      <Flex direction="column" justifyContent="center" alignItems="center">
+        <Image
+          src="/pursuit.png"
+          alt="Pursuit Logo"
+          width="120px"
+          height="auto"
+          mb={4}
+        />
+        <Text mt={4}>&copy; {new Date().getFullYear()} HAUS. <strong>LKHN Technologies</strong> All rights reserved.</Text>
+      </Flex>
     </Box>
   );
 };
--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box } from '@chakra-ui/react';
 import UserNavbar from './Navbars/UserNavbar';
 import AdminNavbar from './Navbars/AdminNavbar';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   const role = localStorage.getItem('role'); // Assume the role is stored in localStorage
@@ -12,6 +13,7 @@ const Layout = ({ children }) => {
       <Box as="main" mt={4}>
         {children}
       </Box>
+      <Footer />
     </Box>
   );
 };
